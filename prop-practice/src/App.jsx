7233e5c91ee207aa9cca2e36c0b67873212d1f00
Augy Markham <katiefaithmarkham@gmail.com>
2025-04-10 11:26:54 -0700

    added components and passed props from App to Hobbies

--- prop-practice/src/App.jsx
+++ prop-practice/src/App.jsx
@@ -1,9 +1,11 @@
+import Hobbies from './components/Hobbies';
 function App() {
 
   const hobbyList = ["Big game hunting", "Roller Skating", "Taxidermy", "Cross stitch", "Watching (people/birds)"];
 
   return (
     <>
+      <Hobbies hobbyList={hobbyList}/>
     </>
   )
 }
